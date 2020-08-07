0630d89aeeaaff08978bfc4235387d045ea34cfe
John T. Wodder II <git@varonathe.org>
2020-08-07 19:42:40 -0400

    Use smaller CC image

--- pelicanconf.py
+++ pelicanconf.py
@@ -109,7 +109,7 @@ else:
 
 FOOTER_HTML = f'''
 <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
-    <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" />
+    <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/80x15.png" />
 </a>
 
 Copyright © {copyright_years} <a xmlns:cc="http://creativecommons.org/ns#"
